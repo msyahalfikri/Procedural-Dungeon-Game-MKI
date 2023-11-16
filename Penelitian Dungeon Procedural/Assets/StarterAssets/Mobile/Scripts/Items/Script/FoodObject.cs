f105aca6029f73725b65767531c47ff44758c749
Zukaze <nico@students.usu.ac.id>
2023-11-16 10:53:00 +0700

    Inventory (II)
    Adding some adjustment to items and inventory

--- Penelitian Dungeon Procedural/Assets/StarterAssets/Mobile/Scripts/Items/Script/FoodObject.cs
+++ Penelitian Dungeon Procedural/Assets/StarterAssets/Mobile/Scripts/Items/Script/FoodObject.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Food Object", menuName = "Inventory System/Items/Food")]
 public class FoodObject :ItemObject
 {
-
+    public int amount;
+    public ItemObject itemStats;
     public int restoreHealthValue;
     public void Awake()
     {
--- Penelitian Dungeon Procedural/Assets/StarterAssets/Mobile/Scripts/Items/Script/ItemObject.cs
+++ Penelitian Dungeon Procedural/Assets/StarterAssets/Mobile/Scripts/Items/Script/ItemObject.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ItemType
+[CreateAssetMenu(fileName = "New Item", menuName = "Create New Item")]
+[System.Serializable]
+public class ItemObject : ScriptableObject
+{
+    public int Id;
+
+    public string itemName;
+
+    public enum ItemType
 {
     Food,
     Equipment,
     Default
 }
-public class ItemObject : ScriptableObject
-{
+    public enum Rarity
+    {
+        common,
+        uncommon,
+        rare,
+        epic,
+        legendary
+    };
+
     public GameObject prefab;
+    public Texture icon;
     public ItemType type;
     [TextArea(15, 20)]
     public string description;
+    public Rarity rarity;
+    public int maxStack;
+    public float weight;
+    public int baseValue;
 }
