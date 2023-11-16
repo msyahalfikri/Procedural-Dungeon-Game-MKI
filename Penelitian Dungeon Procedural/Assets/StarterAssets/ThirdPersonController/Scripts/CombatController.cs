320d138d317af949120ef1b4d7a97458ff036bd2
ariyansikoko <85721035+ariyansikoko@users.noreply.github.com>
2023-11-16 10:26:12 +0700

    Fixed conflicting controller

--- Penelitian Dungeon Procedural/Assets/StarterAssets/ThirdPersonController/Scripts/CombatController.cs
+++ Penelitian Dungeon Procedural/Assets/StarterAssets/ThirdPersonController/Scripts/CombatController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.Windows;
 
 public class CombatController : MonoBehaviour
@@ -12,12 +13,9 @@ public class CombatController : MonoBehaviour
     [SerializeField] private LayerMask mouseColliderMask = new LayerMask();
 
     private StarterAssetsInputs input;
-    private Animator animator;
+    [HideInInspector] public Animator animator;
     public GameObject sword;
-    public GameObject blocksword;
-    bool inAttackAnimation;
-    private CharacterController characterController;
-    public float attackForce = 0.005f;
+    public GameObject blockSword;
 
     // combat combo animation
     public float cooldownTime = 2f;
@@ -29,66 +27,67 @@ public class CombatController : MonoBehaviour
     // combat system
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+    public bool isBlocking = false;
+    public bool isAttacking = false;
+    public bool isExhausted = false;
 
     // Start is called before the first frame update
     void Start()
     {
         input = GetComponent<StarterAssetsInputs>();
         sword = GameObject.FindGameObjectWithTag("Weapon");
-        blocksword = GameObject.FindGameObjectWithTag("Shield");
+        blockSword = GameObject.FindGameObjectWithTag("Shield");
         animator = GetComponent<Animator>();
-        inAttackAnimation = false;
-        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Block();
-        CombatUpdate();
 
-        if (inAttackAnimation == true)
-        {
-            sword.SetActive(true);
-        }
-        else
-        {
-            sword.SetActive(false);
-        }
+        Block();
+
+
+        CombatUpdate();
     }
 
     private void Block()
     {
-        Vector3 mouseWorldPosition = Vector3.zero;
-
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderMask))
+        if (isExhausted)
         {
-            mouseWorldPosition = raycastHit.point;
+            return;
         }
-
-        if (input.block)
-        {
-            blockVirtualCamera.gameObject.SetActive(true);
-            blocksword.SetActive(true);
-
-            animator.SetLayerWeight(2, Mathf.Lerp(animator.GetLayerWeight(2), 1f, Time.deltaTime * 10f));
-
-            Vector3 worldAimTarget = mouseWorldPosition;
-            worldAimTarget.y = transform.position.y;
-            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
-
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
-        }
-
         else
         {
-            blockVirtualCamera.gameObject.SetActive(false);
-            blocksword.SetActive(false);
+            Vector3 mouseWorldPosition = Vector3.zero;
 
-            animator.SetLayerWeight(2, Mathf.Lerp(animator.GetLayerWeight(2), 0f, Time.deltaTime * 10f));
+            Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderMask))
+            {
+                mouseWorldPosition = raycastHit.point;
+            }
+
+
+            if (input.block && !input.attack)
+            {
+                sword.SetActive(false);
+                animator.SetLayerWeight(2, Mathf.Lerp(animator.GetLayerWeight(2), 1f, Time.deltaTime * 10f));
+
+                Vector3 worldAimTarget = mouseWorldPosition;
+                worldAimTarget.y = transform.position.y;
+                Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+                transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
+                isBlocking = true;
+            }
+            else
+            {
+                isBlocking = false;
+                animator.SetLayerWeight(2, Mathf.Lerp(animator.GetLayerWeight(2), 0f, Time.deltaTime * 10f));
+            }
+            blockVirtualCamera.gameObject.SetActive(isBlocking);
+            blockSword.SetActive(isBlocking);
         }
     }
 
@@ -117,12 +116,12 @@ public class CombatController : MonoBehaviour
             animator.SetBool("Attack2", false);
             animator.SetBool("Attack3", false);
             animator.SetBool("IsAttacking", false);
-            inAttackAnimation = false;
+            sword.SetActive(false);
         }
 
         if (Time.time > nextFireTime)
         {
-            if (input.attack)
+            if (input.attack && !input.block)
             {
                 animator.SetBool("IsAttacking", true);
                 OnClick();
@@ -159,7 +158,14 @@ public class CombatController : MonoBehaviour
 
     private void Attack()
     {
-        Vector3 forwardMovement = transform.forward * attackForce;
-        characterController.Move(forwardMovement);
+    }
+    public void StartDealDamage()
+    {
+        sword.GetComponentInChildren<DamageDealer>().StartDealDamage();
+    }
+
+    public void EndDealDamage()
+    {
+        sword.GetComponentInChildren<DamageDealer>().EndDealDamage();
     }
 }
