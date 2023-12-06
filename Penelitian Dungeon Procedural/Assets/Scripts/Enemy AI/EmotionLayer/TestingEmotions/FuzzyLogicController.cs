6ba1e1f6b21622dc4ac7a5ee9df6b37a447664c1
MuhammadSyahAlFikri_056 <fikriibabangdaffa@gmail.com>
2023-12-07 00:42:05 +0700

    AI Development XVII
    +Added working Emotion Simulator with Fuzzy logic with 3 emotions (Anger, Fear, Determination) based on triggers from players and enemy itself
    + Added UI element inform of emojis to represent enemy's emotions [To be Implemented]

---
+++ Penelitian Dungeon Procedural/Assets/Scripts/Enemy AI/EmotionLayer/TestingEmotions/FuzzyLogicController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuzzyLogicController : MonoBehaviour
+{
+    // // Define membership functions and rules here
+
+    // // Example function to evaluate fear based on triggers
+    // public float EvaluateFear(float proximityToPlayer, float healthLevel)
+    // {
+    //     // Evaluate fear using fuzzy logic based on proximity and health level
+    //     float fearValue; /* ... fuzzy logic calculation for fear */
+    //     return fearValue;
+    // }
+    // public float EvaluateAnger(float proximityToPlayer, float healthLevel)
+    // {
+    //     // Evaluate fear using fuzzy logic based on proximity and health level
+    //     float angerValue; /* ... fuzzy logic calculation for anger */
+    //     return angerValue;
+    // }
+    // public float EvaluateDetermination(float proximityToPlayer, float healthLevel)
+    // {
+    //     // Evaluate fear using fuzzy logic based on proximity and health level
+    //     float DeterminationValue; /* ... fuzzy logic calculation for determination */
+    //     return DeterminationValue;
+    // }
+
+}
